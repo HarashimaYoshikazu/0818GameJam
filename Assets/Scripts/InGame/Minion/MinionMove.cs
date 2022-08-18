4dc24f08b9eae0a296389a999ee75723d6212c6c
Harashima <harashima318@gmail.com>
2022-08-19 03:29:00 +0900

    [update]移動の経路探索関数が完成

---
+++ Assets/Scripts/InGame/Minion/MinionMove.cs
@@ -0,0 +1,79 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(MinionParamator))]
+public class MinionMove : MonoBehaviour
+{
+    MinionParamator _minionParamator = null;
+    int _columCount = 0;
+    int _rowCount = 0;
+
+    Cell[,] _cellArray = null;
+    bool[,] _isStepArray  = null;
+    private void Awake()
+    {
+        _minionParamator = GetComponent<MinionParamator>();
+        _columCount = GameManager.Instance.CellManagerInstans.Colm;
+        _rowCount = GameManager.Instance.CellManagerInstans.Row;
+
+        //îzóÒèâä˙âª
+        _cellArray = GameManager.Instance.CellManagerInstans.CellArray;
+        _isStepArray = new bool[_columCount, _rowCount];        
+    }
+
+    private void Start()
+    {
+        CheckAround(_minionParamator.StartPos.Value.x, _minionParamator.StartPos.Value.y);
+    }
+
+    void CheckAround(int colom,int row)
+    {
+        bool isLeft = row == 0;
+        bool isRight = row == _rowCount-1;      
+        bool isTop = colom == 0;
+        bool isBottom  = colom == _columCount-1;
+
+        if (!isLeft)//ç∂
+        {
+            if ((_cellArray[colom , row - 1].CurrentCellType ==CellTypes.Load|| _cellArray[colom , row - 1].CurrentCellType == CellTypes.HeroArea) && !_isStepArray[colom , row - 1])
+            {
+                _isStepArray[colom , row - 1] = true;
+                Move(colom, row - 1);
+                return;
+            }            
+        }
+        if (!isRight)
+        {
+            if ((_cellArray[colom , row + 1].CurrentCellType == CellTypes.Load || _cellArray[colom , row + 1].CurrentCellType == CellTypes.HeroArea) && !_isStepArray[colom , row + 1])
+            {
+                _isStepArray[colom , row + 1] = true;
+                Move(colom, row + 1);
+                return;
+            }
+        }
+        if (!isTop)
+        {
+            if ((_cellArray[colom - 1, row].CurrentCellType == CellTypes.Load || _cellArray[colom - 1, row].CurrentCellType == CellTypes.HeroArea) && !_isStepArray[colom - 1, row ])
+            {
+                _isStepArray[colom - 1, row] = true;
+                Move(colom - 1, row);
+                return;
+            }
+        }
+        if (!isBottom)
+        {
+            if ((_cellArray[colom + 1, row ].CurrentCellType == CellTypes.Load || _cellArray[colom + 1, row ].CurrentCellType == CellTypes.HeroArea) && !_isStepArray[colom + 1, row ])
+            {
+                _isStepArray[colom + 1, row ] = true;
+                Move(colom + 1, row);
+                return;
+            }
+        }
+    }
+
+    void Move(int colom, int row)
+    {
+        this.transform.SetParent(_cellArray[row,colom].transform);
+    }
+}
--- Assets/Scripts/InGame/Minion/MinionParamator.cs
+++ Assets/Scripts/InGame/Minion/MinionParamator.cs
@@ -2,7 +2,73 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/// <summary>
+/// パラメータを格納するクラス
+/// </summary>
 public class MinionParamator : MonoBehaviour
 {
-    
+    Vector2Int? _startPos = null;
+    public Vector2Int? StartPos
+    {
+        get { return _startPos; }
+        set
+        {
+            _startPos = value;
+            _currentPos = value;
+        }
+    }
+
+    Vector2Int? _currentPos = null;
+    public Vector2Int? CurrentPos
+    {
+        get
+        {
+            if (_currentPos == null && this.transform.parent.TryGetComponent<Cell>(out Cell cell))
+            {
+                if (cell.CellPos == null)
+                {
+                    throw new System.ArgumentNullException("生成時に座標が正しくセットされていないか、オブジェクトの名称が不正です。");
+                }
+                _currentPos = cell.CellPos;
+            }
+            return _currentPos;
+        }
+        set { _currentPos = value; }
+    }
+
+    [SerializeField]
+    int _atk = 0;
+    public int Atk
+    {
+        get { return _atk; }
+        set { _atk = Mathf.Clamp(_atk + value, 0, _atk + value); }
+    }
+
+    [SerializeField]
+    int _hP = 0;
+    public int HP
+    {
+        get { return _hP; }
+        set
+        {
+            _hP = Mathf.Clamp(_hP + value, 0, _hP + value);
+            if (_hP <= 0)
+            {
+                Death();
+            }
+        }
+    }
+
+    [SerializeField]
+    float _moveTime = 0f;
+    public float MoveTime
+    {
+        get { return _moveTime; }
+        set { _moveTime = Mathf.Clamp(_moveTime + value, 0, _moveTime + value); }
+    }
+
+    void Death()
+    {
+        Destroy(this.gameObject);
+    }
 }
